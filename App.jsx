ef601f3182d836af608b4bc04600d0f2da183f8d
Nawang Juwita Asmara Dewati <119095730+wangsssjuww@users.noreply.github.com>
2024-07-07 16:58:16 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { Route, Routes} from 'react-router-dom';
+import Home from './Pages/Home';
+import LandingPage from './komponen/LandingPage';
+import Landing from './Pages/Landing';
+import State from './Pages/state';
+import EvenHandling from './Pages/handling';
+import UseEffect from './Pages/UseEffect';
+import Animasi from './Pages/timer';
+import FetchApi from './Pages/fetch';
+import ReactContext from './Pages/reactcontext';
+
+
+
+function App () {
+  return (
+    <>
+      <Routes>
+        <Route path="/" Component={Home}/>
+        <Route path="/landingpage" Component={Landing}/>
+        <Route path="/state" Component={State}/>
+        <Route path="/handling" Component={EvenHandling}/>
+        <Route path="/effect" Component={UseEffect}/>
+        <Route path="/seconds" Component={Animasi}/>
+        <Route path="/fetch" Component={FetchApi}/>
+        <Route path="/react" Component={ReactContext}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
